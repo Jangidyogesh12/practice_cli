6b6b48e4350aaace4bcbd27c7c143b23aba846b7
Jangidyogesh12 <jangidyogesh123@gmail.com>
2026-02-12 17:47:39 +0530

    first commit

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const commander_1 = require("commander");
-const hello = () => {
-    console.log("Hello, world!");
-};
-const program = new commander_1.Command();
-program.command("hello").description("Say hello").action(hello);
-program.parse(process.argv);
